2b58f11aac8318e9b76804b3c28b898e313bbcb7
Weronika Szybińska <92526994+szyba0102@users.noreply.github.com>
2022-03-18 21:00:43 +0100

    Update quicksort_hoare.py

--- sorting aglorithms/quicksort_hoare.py
+++ sorting aglorithms/quicksort_hoare.py
@@ -29,7 +29,3 @@ def quick_sort(A, p, r):
             quick_sort(A, q+1, r)
             r = q - 1
 
-
-T = [1,4,54,24,5,7,53,42,35,5,45,24,5,4,2,5,567,4,32,234,5543,4]
-quick_sort(T,0,len(T)-1)
-print(T)
