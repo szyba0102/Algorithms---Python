d8310d9a58aec0309d6ac856b8637190bf09d16c
Weronika Szybińska <92526994+szyba0102@users.noreply.github.com>
2022-03-18 21:37:01 +0100

    Update magic_fives.py

--- sorting aglorithms/magic_fives.py
+++ sorting aglorithms/magic_fives.py
@@ -74,17 +74,4 @@ def linearselect( A, k ): #wywolanie glownej funkcji
 
 
 
-seed(42)
-
-n = 11
-for i in range(n):
-  A = list(range(n))
-  shuffle(A)
-  print(A)
-  x = linearselect( A, i )
-  if x != i:
-    print("Blad podczas wyszukiwania liczby", i)
-    exit(0)
-    
-print("OK")
 
